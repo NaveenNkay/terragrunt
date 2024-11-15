113fdb7acd9d01d7f283ea304efc4208a9a0ce3d
NaveenNkay <147308581+NaveenNkay@users.noreply.github.com>
2024-11-15 12:12:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "project_name" {
   description = "The name of the GCP project"
   type        = string
-  default     = "quick-assesment"
+  default     = "python-test-441805"
 }
 
 variable "region" {
   description = "The region for the VPC"
   type        = string
-  default     = "asia-east1"
+  default     = "asia-south1"
 }
 
 variable "subnets" {
@@ -20,36 +20,36 @@ variable "subnets" {
     services_ip_range = string
   }))
   default = {
-    "subnet_100" = {
-      name          = "subnet-100"
-      region        = "asia-east1"
-      ip_cidr_range = "10.10.0.0/24"
-      pods_ip_range   = "10.10.1.0/24"  # Pods range
-      services_ip_range = "10.10.2.0/24"  # Services range
+    "subnet_p100" = {
+      name          = "subnet-p100"
+      region        = "asia-south1"
+      ip_cidr_range = "10.100.0.0/24"
+      pods_ip_range   = "10.100.1.0/24"  # Pods range
+      services_ip_range = "10.100.2.0/24"  # Services range
     },
-    "subnet_200" = {
-      name          = "subnet-200"
-      region        = "europe-west1"
-      ip_cidr_range = "10.11.0.0/24"
-      pods_ip_range  = "10.11.1.0/24"  # Pods range
-      services_ip_range = "10.11.2.0/24"  # Services range
+    "subnet_p200" = {
+      name          = "subnet-p200"
+      region        = "asia-south1"
+      ip_cidr_range = "10.110.0.0/24"
+      pods_ip_range  = "10.110.1.0/24"  # Pods range
+      services_ip_range = "10.110.2.0/24"  # Services range
     }
   }
 }
 variable "zone" {
   description = "The zone where the GKE cluster control plane will be created"
   type        = string
-  default     = "asia-east1-a"
+  default     = "asia-south1-a"
 }
 
 variable "node_zones" {
   description = "List of zones for the GKE node pool"
   type        = list(string)
-  default     = ["asia-east1-a", "asia-east1-b", "asia-east1-c"]
+  default     = ["asia-south1-a", "asia-south1-b", "asia-south1-c"]
 }
 
 variable "cluster_name" {
   description = "The name of the GKE cluster"
   type        = string
-  default     = "private-gke-cluster"
+  default     = "private-gke-cluster-2"
 }
