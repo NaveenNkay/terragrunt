78d11493c12937cc2582ffa336f3afdd562ea296
NaveenNkay <147308581+NaveenNkay@users.noreply.github.com>
2024-11-15 12:37:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_compute_address" "nat-address" {
   name         = "nat-address"
   address_type = "EXTERNAL"
-  region  = asia-south1
+  region  = "asia-south1"
   network_tier = "PREMIUM"
 }
